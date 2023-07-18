654a6d37ffaa5fb030dc12d077e8fb6ef71bfef2
imran <imranpranto.official@gmail.com>
2023-07-18 15:47:22 +0600

    remove other pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
-import { useState } from "react";
 import "./App.css";
 import ToDo from "./components/ToDo";
-import ToDoapp from "./components/ToDoapp";
-import To from "./components/To";
 
 function App() {
   return (
     <>
       <ToDo />
-      {/* <ToDoapp />
-      <To /> */}
     </>
   );
 }
